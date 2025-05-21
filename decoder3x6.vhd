d9dae12d2e6a28c79f8afb271a9546f30f9b0a9c
Vinicius <viniciusferreiramatias79@gmail.com>
2025-05-21 15:11:55 -0300

    modificação no decoder

--- decoder3x6.vhd
+++ decoder3x6.vhd
@@ -4,24 +4,22 @@ use ieee.numeric_std.all;
 
 entity decoder3x6 is
     port(
-         sel:in unsigned(4 downto 0);
-         s0: out std_logic;
-         s1: out std_logic;
-         s2: out std_logic;
-         s3: out std_logic;
-         s4: out std_logic;
-         s5: out std_logic
-         );
+        sel: in unsigned(4 downto 0);
+        s0 : out std_logic;
+        s1 : out std_logic;
+        s2 : out std_logic;
+        s3 : out std_logic;
+        s4 : out std_logic;
+        s5 : out std_logic
+    );
 end entity;
 
 architecture a_decoder3x6 of decoder3x6 is
-
 begin
-    s0 <= not(sel(0)) and not(sel(1)) and not(sel(3));
-    s1 <= sel(0) and not(sel(1)) and not(sel(2));
-    s2 <= not(sel(0)) and sel(1) and not(sel(2));
-    s3 <= sel(0) and sel(1) and not(sel(2));
-    s4 <= not(sel(0)) and not(sel(1)) and sel(2);
-    s5 <= sel(0) and not(sel(1)) and sel(2);
-
+    s0 <= '1' when sel = "00000" else '0';
+    s1 <= '1' when sel = "00001" else '0';
+    s2 <= '1' when sel = "00010" else '0';
+    s3 <= '1' when sel = "00011" else '0';
+    s4 <= '1' when sel = "00100" else '0';
+    s5 <= '1' when sel = "00101" else '0';
 end architecture;
