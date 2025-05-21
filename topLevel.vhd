43a1071aca561858e213d470bf7e401cf8d1e650
Vinicius <viniciusferreiramatias79@gmail.com>
2025-05-21 15:10:42 -0300

    mudei topLevel

--- topLevel.vhd
+++ topLevel.vhd
@@ -12,6 +12,9 @@ entity topLevel is
          const    : in unsigned(15 downto 0);
          reg_wr   : in unsigned(4 downto 0);
          reg_r1   : in unsigned(4 downto 0);
+         data_wr_bRegs_sel :in unsigned(1 downto 0); --seleciona fonte de dados para o banco de registradores
+         A_wr_sel : in unsigned(1 downto 0); --seleciona fonte do dado a escrever no A
+         A_we     : in std_logic;            --habilita escrita no acumulador
          overflow : out std_logic;
          negativo : out std_logic;
          zero     : out std_logic
@@ -35,8 +38,8 @@ architecture a_topLevel of topLevel is
     component ULA
         port
         (
-            rg1 : in  unsigned (15 downto 0);
-            rg2 : in  unsigned (15 downto 0);
+            rg1 : in  unsigned (15 downto 0); -- usado como A
+            rg2 : in  unsigned (15 downto 0); -- possivel cte
             sel : in  unsigned  (1 downto 0);
             rg_out : out unsigned (15 downto 0);
             Z   : out std_logic;
@@ -56,18 +59,39 @@ architecture a_topLevel of topLevel is
     end component;
 
 
-    signal result, A_in, A_out, rg1: unsigned(15 downto 0);
-    signal A_wr_en: std_logic;
+    signal result, A_in, A_out, rg1, data_wr_bRegs: unsigned(15 downto 0);
 
     begin
 
         A: reg16bits
-        port map (clk=>clk, rst=>rst, wr_en=>A_wr_en, data_in=>A_in, data_out=>A_out);
+        port map (clk=>clk, rst=>rst, wr_en=>A_we, data_in=>A_in, data_out=>A_out);
 
         banco: bancoRegs
-        port map(clk=>clk, rst=>rst, wr_en=>wr_en, data_wr=>data_wr, reg_wr=>reg_wr, reg_r1=>reg_r1, data_r1=>rg1);
+        port map (
+                  clk=>clk, rst=>rst, 
+                  wr_en=>wr_en, data_wr=>data_wr_bRegs,
+                  reg_wr=>reg_wr, reg_r1=>reg_r1, 
+                  data_r1=>rg1
+                 );
 
         ULA0: ULA
-        port map(rg1 => rg1, rg2 => A_out, sel => ula_op, rg_out => result, Z => zero, N => negativo, V => overflow);
+        port map(
+                rg1 => A_out,      -- acumulador
+                rg2 => rg1,        -- segundo operando banco
+                sel => ula_op,
+                rg_out => result,
+                Z => zero, N => negativo, V => overflow
+                );
+
+        A_in <= result when A_wr_sel = "00" else
+                const  when A_wr_sel = "01" else
+                data_r1 when A_wr_sel = "10" else
+                data_wr when A_wr_sel = "11" else
+                "0000000000000000";
+
+        data_wr_bRegs <= data_wr when data_wr_bRegs_sel = "00" else
+                         const when data_wr_bRegs_sel = "01" else
+                         A_out when data_wr_bRegs_sel = "10" else
+                         "0000000000000000";
 
 end architecture;
