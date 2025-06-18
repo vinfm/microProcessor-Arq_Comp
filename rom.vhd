afa65739bc9fa47969f335a867a2f3fddaeb87a2
Vinicius <viniciusferreiramatias79@gmail.com>
2025-06-17 21:35:05 -0300

    correção assembly atual e instruções na rom

--- rom.vhd
+++ rom.vhd
@@ -16,11 +16,11 @@ architecture a_rom of rom is
       2  => B"1101_111_100_000000", -- sw r4, A
       3  => B"0011_010_111_000000", -- lw r2, A
       4  => B"0101_111_100_111_111", -- soma A com R4
-      5  => B"0100_100_111_101010", -- carrega o valor de A no registrador R4
+      5  => B"0101_100_111_101_001", -- carrega o valor de A no registrador R4
       6  => B"0010_111_000000001", -- carrega o valor 1 no acumulador A
       7  => B"0101_111_011_111_111", -- soma A com R3
-      8  => B"0100_011_111_010101", -- carrega o valor do acumulador em R3
-      9  => B"0100_111_011_110011", -- carrega o valor de R3 no acumulador A
+      8  => B"0101_011_111_010_001", -- carrega o valor do acumulador em R3
+      9  => B"0101_111_011_110_001", -- carrega o valor de R3 no acumulador A
       10  => B"1001_111_000011110", -- compara o valor do acumulador A com o valor 30 (11110)-mudei para 1
       11 => B"1100_00000010_1_101", -- branch para o endereço 12 se A for maior ou igual 
       12 => B"1111_01010_0000010", -- jump para o endereço 2
