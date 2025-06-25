d21eaedcd7d262e1b78801bd6b4832c234950587
Vinicius <viniciusferreiramatias79@gmail.com>
2025-06-25 20:57:13 -0300

    mudança no processador testbench

--- tbs/processador_tb.vhd
+++ tbs/processador_tb.vhd
@@ -9,20 +9,23 @@ end entity;
 architecture a_processador_tb of processador_tb is
     component processador
         port(        
-        clk             : in std_logic; --clock
-        rst             : in std_logic --reset geral
+        clk       : in  std_logic;            --clock
+        rst       : in  std_logic;            --reset geral
+        bus_debug : out unsigned(15 downto 0) --bus de debug para visualização
             );
     end component;
 
     signal clk        : std_logic := '0';
     signal rst        : std_logic := '1';
+    signal bus_debug  : unsigned(15 downto 0);
 
 begin
 
     uut: processador
         port map(
             clk             => clk,
-            rst             => rst
+            rst             => rst,
+            bus_debug       => bus_debug
         );
 
     -- Clock de 10ns
@@ -30,7 +33,7 @@ begin
     begin
 
         wait for 20 ns; -- Atraso inicial para garantir que o reset seja aplicado antes do clock
-        while now < 55000 ns loop
+        while now < 200000 ns loop
             clk <= '0';
             wait for 10 ns;
             clk <= '1';
